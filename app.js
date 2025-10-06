4ddd1c5058d6d0beffe24be5310abd310f92f914
Alaa Rajab <alaa88rajab@gmail.com>
2025-10-06 11:49:12 -0500

    changes

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const port = process.env.PORT || 3001;
 // MongoDB connection
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  // eslint-disable-next-line no-console
   .then(() => console.log("Connected to MongoDB"))
   .catch(console.error);
 app.use((req, res, next) => {
@@ -25,6 +26,7 @@ app.use((req, res, next) => {
 app.use("/", mainRouter);
 
 // Start server
+// eslint-disable-next-line no-console
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
