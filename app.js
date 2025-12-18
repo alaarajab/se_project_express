6704f710c3c881393db0013745757f77fedc4539
Alaa Rajab <alaa88rajab@gmail.com>
2025-12-18 12:50:32 -0600

    Add /crash-test route for PM2 crash recovery review

--- app.js
+++ app.js
@@ -16,13 +16,6 @@ app.use(cors());
 app.use(express.json());
 
 // 🔹 Crash-test route (FOR REVIEW ONLY)
-app.get("/crash-test", (req, res) => {
-  setTimeout(() => {
-    throw new Error("Server will crash now"); // simulate crash
-  }, 0);
-
-  res.send("Server will crash shortly (check PM2 logs)");
-});
 
 //Enable request logger BEFORE all routes
 app.use(requestLogger);
